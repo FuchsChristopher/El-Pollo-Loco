4f89a0f0ba8345f4c11927b1e1f57560952659ab
Christopher Fuchs <fuchs-christopher@gmx.de>
2023-03-27 17:56:06 +0200

    worked on ell pollo finished creat objects

--- models/character.class.js
+++ models/character.class.js
@@ -1,5 +1,6 @@
 class Character extends MovableObjekt{
-
+    height = 285;
+    y = 150;
 
     constructor() {
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');
--- models/chicken.class.js
+++ models/chicken.class.js
@@ -1,5 +1,7 @@
 class Chicken extends MovableObjekt {
-
+    y = 357;
+    height = 65;
+    width = 90;
 
     constructor() {
         super().loadImage('img/3_enemies_chicken/chicken_normal/1_walk/1_w.png');
